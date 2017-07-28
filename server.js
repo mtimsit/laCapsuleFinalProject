8ac83c4b8e2ff84925a4c5c2fd12ac2e66ecf8c9
Michael Timsit <michael.timsit@gmail.com>
2017-07-28 11:57:24 +0200

    v0.0.2

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var express = require('express');
 var app = express();
 
 //Allow cross domain request
-app.use(function(req, res) {
+aapp.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 //npm install body-parser --save

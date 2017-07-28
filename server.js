f6bdc185b0e9353364b4bfc2f05ec24e02a4118e
Michael Timsit <michael.timsit@gmail.com>
2017-07-28 11:50:15 +0200

    v0.0.2

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 //Allow cross domain request
 app.use(function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
